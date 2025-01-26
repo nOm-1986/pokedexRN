94800b5255ddc8485f6187211613a16f56df944c
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-01-25 19:11:24 -0500

    Go back done

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,4 +1,5 @@
 import { ScrollView } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
 import React, { useState, useEffect } from 'react'
 import { getPokemonDetailsApi} from '../api/pokemon'
 import Header from '../components/Pokemon/Header';
@@ -7,7 +8,14 @@ import Stats from '../components/Pokemon/Stats'
 
 export default function Pokemon(props) {
   const {route: { params }, navigation } = props;
-  const [pokemon, setPokemon] = useState(null)
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => <Icon name="arrow-left" color="#fff" size={18} style={{ marginLeft: 20 }} onPress={navigation.goBack}/> 
+    })
+  }, [navigation, params])
   
   useEffect(() => {
     (async () => {
